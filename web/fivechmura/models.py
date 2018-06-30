942356f48cf5cd939aa6e1c3eaafee405bc8ce09
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-06-30 19:00:56 +0900

    tweetモデル作成

--- web/fivechmura/models.py
+++ web/fivechmura/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Tweet(models.Model):
+    owner_name = models.CharField(max_length=20)
+    tweet_text = models.CharField(max_length=400)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return 'name:' + self.owner_name + 'tweet:' + self.tweet_text
--- web/py_haru/settings.py
+++ web/py_haru/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'fivechmura.apps.FivechmuraConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
