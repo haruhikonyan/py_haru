0aac243734e81f5b3f16c7da201bc69b3403d302
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-06-30 19:40:33 +0900

    つぶやきが表示できるように

--- web/fivechmura/models.py
+++ web/fivechmura/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Tweet(models.Model):
-    owner_name = models.CharField(max_length=20)
-    tweet_text = models.CharField(max_length=400)
+    owner_name = models.CharField(max_length=20, default='名無し村人')
+    tweet_text = models.CharField(max_length=1000)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
-        return 'name:' + self.owner_name + 'tweet:' + self.tweet_text
+        return '名前:' + self.owner_name + '\n本文:\n' + self.tweet_text
--- web/fivechmura/views.py
+++ web/fivechmura/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Tweet
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, 5channeler ここにtwitterみたいなのを作ろうと思う")
+    tweet_list = Tweet.objects.all()
+
+    template = loader.get_template('index.html')
+    context = {
+        'tweet_list': tweet_list,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 def detail(request, tweet_id):
-    return HttpResponse("You're looking at tweet %s." % tweet_id)
+    tweet = Tweet.objects.get(id=tweet_id)
+
+    template = loader.get_template('tweet/detail.html')
+    context = {
+        'tweet': tweet,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 def tweet(request, tweet_id):
     return HttpResponse("You're tweet %s." % tweet_id)
