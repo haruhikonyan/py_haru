505b9c95e26535326bcf2e88be824e28a7f94ff3
haruhiko <haruhiko1jp@yahoo.co.jp>
2018-06-30 19:07:22 +0900

    WIP tweet詳細ページ追加

--- web/fivechmura/urls.py
+++ web/fivechmura/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /tweet/5/
+    path('tweet/<int:tweet_id>/', views.detail, name='detail'),
 ]
--- web/fivechmura/views.py
+++ web/fivechmura/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, 5channeler")
+    return HttpResponse("Hello, 5channeler ここにtwitterみたいなのを作ろうと思う")
+
+def detail(request, tweet_id):
+    return HttpResponse("You're looking at tweet %s." % tweet_id)
+
+def tweet(request, tweet_id):
+    return HttpResponse("You're tweet %s." % tweet_id)
